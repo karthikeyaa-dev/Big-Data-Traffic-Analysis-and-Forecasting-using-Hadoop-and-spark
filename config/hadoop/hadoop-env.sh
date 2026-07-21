d8a73919d124ec6ac05c5e3ffdff45e785224f42
karthikeyaa-dev <karthikeyaa.official@gmail.com>
2026-07-21 19:44:58 +0530

    This is the intial commit and in this commit we configured the hadoop for psudeo configuraton

---
+++ config/hadoop/hadoop-env.sh
@@ -0,0 +1,2 @@
+export HADOOP_LOG_DIR=/home/karthikeya/Development/Machine_Learning/Traffic_Analytics/config/logs
+export JAVA_HOME=/nix/store/04cqkgis3i4b0fm42h5s77i96dp9gmyg-openjdk-17.0.20+2/lib/openjdk
